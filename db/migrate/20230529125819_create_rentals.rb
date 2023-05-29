e4af3e7f7ef3dce620565b403eeda85a53721003
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-05-29 15:05:41 +0200

    ajout adresse et modification status

--- db/migrate/20230529125819_create_rentals.rb
+++ db/migrate/20230529125819_create_rentals.rb
@@ -1,7 +1,7 @@
 class CreateRentals < ActiveRecord::Migration[7.0]
   def change
     create_table :rentals do |t|
-      t.boolean :status
+      t.string :status
       t.date :rental_begin
       t.date :rental_end
       t.references :user, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_125819) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_130309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_125819) do
   end
 
   create_table "rentals", force: :cascade do |t|
-    t.boolean "status"
+    t.string "status"
     t.date "rental_begin"
     t.date "rental_end"
     t.bigint "user_id", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_125819) do
     t.string "user_name"
     t.string "first_name"
     t.string "last_name"
+    t.string "adress"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
