5e1394ef6b520c490aada2aa038c62dea5822511
evans hulot <hulotevans@gmail.com>
2023-05-29 16:52:13 +0200

    Had validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :dwarfs
+  has_many :rentals
+
+  validates :first_name, :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
