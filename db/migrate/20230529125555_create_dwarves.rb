7b6da40772b264c3525a6fd85cbc994b7d2bc2f8
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-05-29 14:59:03 +0200

    creation models

---
+++ db/migrate/20230529125555_create_dwarves.rb
@@ -0,0 +1,13 @@
+class CreateDwarves < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dwarves do |t|
+      t.string :name
+      t.text :description
+      t.integer :age
+      t.string :location
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
