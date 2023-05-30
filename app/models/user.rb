a0718f5c7ecdc7424869bdee7a96c36639267bdf
evans hulot <hulotevans@gmail.com>
2023-05-30 14:07:26 +0200

    started the index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :dwarfs
   has_many :rentals
 
-  validates :user_name, :first_name, :last_name, :address, presence: true
+  # validates :user_name, :first_name, :last_name, :address, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
