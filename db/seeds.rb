4f4790e6fd38934a65c31e0a044812df854791af
Jerry Buck <jerry.buck@pt.lu>
2023-05-30 17:07:17 +0200

    Added seeds for User an Dwarfes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+seeding_users = ["jerryphilippe", "evanshulot", "leeloom", "ManueGI"]
+
+seeding_users.each do |user_name|
+  url = "https://api.github.com/users/#{user_name}"
+  user_serialized = URI.open(url).read
+  g_user = JSON.parse(user_serialized)
+  user = User.new
+  user.user_name = g_user['login']
+  g_user['name'].nil? ? name = "No Name".split : name = g_user['name'].split
+  user.email = "#{name.first}@nain.com"
+  user.first_name = name.first
+  user.last_name = name.last
+  user.address = "68 Ave Parmentier 75011 Paris"
+  user.password = '123456'
+  user.password_confirmation = '123456'
+  user.save!
+  img = URI.open(g_user['avatar_url'])
+  user.avatar.attach(io: img, filename: "#{g_user['login']}.jpeg", content_type: "image/jpeg")
+  dwarf = Dwarf.new
+  dwarf.name = "Dwarf-#{name.first}"
+  dwarf.description = "Dwarf-#{name.first} is a very nice dwarf will fit in every garden"
+  dwarf.age = rand(5..99)
+  dwarf.address = "16 Villa Gaudelet, 75011 Paris"
+  dwarf.user_id = user.id
+  dwarf.save!
+  img = File.open("app/assets/images/#{g_user['login']}.png")
+  dwarf.photo.attach(io: img, filename: "#{g_user['login']}.jpeg", content_type: "image/jpeg")
+end
