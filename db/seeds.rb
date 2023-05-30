b63ce29e9bdd183301dd4012792552385c9506a1
evans hulot <hulotevans@gmail.com>
2023-05-30 16:22:25 +0200

    finish index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Dwarf.destroy_all
+# User.create!(email: "evans@gmails.com", password: "bonjour")
+user = User.new(email: "hulotevans@gmail.com", password: "bonjour")
+user.save!
+a = Dwarf.new(name: "toto", description: "great description", age: "38", address: "paris")
+a.user = user
+a.save!
+b = Dwarf.new(name: "toto", description: "great description", age: "38", address: "paris")
+b.user = user
+b.save!
+c = Dwarf.new(name: "toto", description: "great description", age: "38", address: "paris")
+c.user = user
+c.save!
+d = Dwarf.new(name: "toto", description: "great description", age: "38", address: "paris")
+d.user = user
+d.save!
+e = Dwarf.new(name: "toto", description: "great description", age: "38", address: "paris")
+e.user = user
+e.save!
+f = Dwarf.new(name: "toto", description: "great description", age: "38", address: "paris")
+f.user = user
+f.save!
