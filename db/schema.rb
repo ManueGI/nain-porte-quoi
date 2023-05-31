f85ad9cb66e8a4c02384010af5172b8408797206
evans hulot <hulotevans@gmail.com>
2023-05-31 15:51:02 +0200

    show style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_130309) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_114621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_130309) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_dwarves_on_user_id"
   end
 
