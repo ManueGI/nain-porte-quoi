07886e01763d117c829dbf86941416e832c8e079
Jerry Buck <jerry.buck@pt.lu>
2023-06-01 14:35:53 +0200

    Added Reviews to show and index and pop form for reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_114621) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_115413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_114621) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "content"
-    t.float "score"
+    t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "rental_id"
